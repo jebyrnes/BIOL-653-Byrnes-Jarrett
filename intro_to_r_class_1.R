533575641af4b7e713c3e237479a66f36e5658e6
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2015-09-15 09:32:33 -0400

    Basics done

---
+++ intro_to_r_class_1.R
@@ -0,0 +1,40 @@
+#Calculator
+2 + 2
+
+#order of operations
+3 + 4 * 2
+
+(3 + 4) / 2
+
+
+#scientific notation
+1e5
+100000
+10^5
+
+#mathematic functions
+log(2) #natural log
+log10(2) #base 10 log
+log(2, base=2) #base 2 log
+
+exp(log(2))
+
+
+#Comparisons
+
+2 == 2
+2 == 3
+
+
+#not equal
+2 != 3
+2 != 2
+
+#greater than, less that
+
+2 < 3
+2 > 3
+
+2 < 2
+2 <= 2
+2 >= 3
